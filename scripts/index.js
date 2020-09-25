1005be0e48040efda0d9629fcea45c86dc6bf96d
Carmen Fajardo <cfajardo440@gmail.com>
2020-09-25 14:01:13 +0200

    Second commit

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// Elemento
+
+    // establecer las celdas de 0 a 8
+    // tienen que aparecer los movientos en la pantalla
+    // siempre se inica el juego con la X
+
+
+// Evento
+
+   // la lógica sería si 0-1-2, 3-4-5, 6,7,8, hasta la útima combinacion ganadora  son true, todo el resto es false. Buscar fórmula
+   // desabilitar  el contenido de la casilla una vez este ocupada
+
+
+// Ejecución
+
